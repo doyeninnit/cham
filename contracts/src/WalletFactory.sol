ae3d3c8cc33b9acc4e7e9f3712f20b9cc386c8fc
doyeninnit <agatenashons@gmail.com>
2024-06-02 14:59:54 +0300

    changes

--- contracts/src/WalletFactory.sol
+++ contracts/src/WalletFactory.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.12;
 
 import {IEntryPoint} from "account-abstraction/interfaces/IEntryPoint.sol";
-import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
-import {Create2} from "@openzeppelin/contracts/utils/Create2.sol";
 import {Wallet} from "./Wallet.sol";
+import {Create2} from "@openzeppelin/contracts/utils/Create2.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 contract WalletFactory {
     Wallet public immutable walletImplementation;
@@ -13,53 +13,41 @@ contract WalletFactory {
         walletImplementation = new Wallet(entryPoint, address(this));
     }
 
+    function createAccount(
+        address[] memory owners,
+        uint256 salt
+    ) external returns (Wallet) {
+        address addr = getAddress(owners, salt);
+        uint256 codeSize = addr.code.length;
+        if (codeSize > 0) {
+            return Wallet(payable(addr));
+        }
 
-function getAddress(
-    address[] memory owners,
-    uint256 salt
-) public view returns (address) {
-    // Encode the initialize function in our wallet with the owners array as an argument into a bytes array
-    bytes memory walletInit = abi.encodeCall(Wallet.initialize, owners);
-    // Encode the proxyContract's constructor arguments which include the address walletImplementation and the walletInit
-    bytes memory proxyConstructor = abi.encode(
-        address(walletImplementation),
-        walletInit
-    );
-    // Encode the creation code for ERC1967Proxy along with the encoded proxyConstructor data
-    bytes memory bytecode = abi.encodePacked(
-        type(ERC1967Proxy).creationCode,
-        proxyConstructor
-    );
-    // Compute the keccak256 hash of the bytecode generated
-    bytes32 bytecodeHash = keccak256(bytecode);
-    // Use the hash and the salt to compute the counterfactual address of the proxy
-    return Create2.computeAddress(bytes32(salt), bytecodeHash);
-}
+        bytes memory walletInit = abi.encodeCall(Wallet.initialize, owners);
+        ERC1967Proxy proxy = new ERC1967Proxy{salt: bytes32(salt)}(
+            address(walletImplementation),
+            walletInit
+        );
 
-function createAccount(
-    address[] memory owners,
-    uint256 salt
-) external returns (Wallet) {
-    // Get the counterfactual address
-    address addr = getAddress(owners, salt);
-    // Check if the code at the counterfactual address is non-empty
-    uint256 codeSize = addr.code.length;
-    if (codeSize > 0) {
-        // If the code is non-empty, i.e. account already deployed, return the Wallet at the counterfactual address
-        return Wallet(payable(addr));
+        return Wallet(payable(address(proxy)));
     }
 
-    // If the code is empty, deploy a new Wallet
-    bytes memory walletInit = abi.encodeCall(Wallet.initialize, owners);
-    ERC1967Proxy proxy = new ERC1967Proxy{salt: bytes32(salt)}(
-        address(walletImplementation),
-        walletInit
-    );
+    function getAddress(
+        address[] memory owners,
+        uint256 salt
+    ) public view returns (address) {
+        bytes memory walletInit = abi.encodeCall(Wallet.initialize, owners);
+        bytes memory proxyConstructor = abi.encode(
+            address(walletImplementation),
+            walletInit
+        );
+        bytes memory bytecode = abi.encodePacked(
+            type(ERC1967Proxy).creationCode,
+            proxyConstructor
+        );
 
-    // Return the newly deployed Wallet
-    return Wallet(payable(address(proxy)));
-}
+        bytes32 bytecodeHash = keccak256(bytecode);
 
-
-
+        return Create2.computeAddress(bytes32(salt), bytecodeHash);
+    }
 }
